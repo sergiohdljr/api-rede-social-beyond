7dd966a10fb3e859af21678162719a04fc491172
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-21 15:42:15 -0300

    setup: express config

--- src/server.js
+++ src/server.js
@@ -1,12 +1,6 @@
-import express from "express";
+import app from "./app";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
