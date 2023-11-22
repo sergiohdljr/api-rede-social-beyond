6d2f5288ffb4d6ab81475d99a27e7d4649859f87
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-22 14:39:25 -0300

    fix: remove the console.log

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
-import app from "./app";
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT);
