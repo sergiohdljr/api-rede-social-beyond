efe0ff87cac91f09b272ecadb209aa91cd1730f6
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-22 15:07:16 -0300

    feat: setup firebase admin

---
+++ src/db/firestore.js
@@ -0,0 +1,10 @@
+import * as admin from "firebase-admin";
+import * as serviceAccount from "./projeto-beyond-8c339-firebase-adminsdk-drpgc-ed3497b204.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+export { db };
