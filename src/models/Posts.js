bbf0a7dc445690697a3a5d4a451e121f7f69b0a0
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-22 15:37:45 -0300

    feat: create User and Posts Models

---
+++ src/models/Posts.js
@@ -0,0 +1,12 @@
+class Post {
+  constructor(data, foto, horario, id, texto, user) {
+    this.id = id;
+    this.data = data;
+    this.foto = foto;
+    this.horario = horario;
+    this.texto = texto;
+    this.user = user;
+  }
+}
+
+export default Post;
---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+class User {
+  constructor(fotoPerfil, nome, email) {
+    this.fotoPerfil = fotoPerfil;
+    this.nome = nome;
+    this.email = email;
+  }
+}
+
+export default User;
