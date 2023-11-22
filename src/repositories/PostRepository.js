28c462eb9b9e2c170b309d37ef2dbea6767f3fdb
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-22 17:48:52 -0300

    feat: create findAllPosts from PostRepository

---
+++ src/repositories/PostRepository.js
@@ -0,0 +1,24 @@
+import db from "../db/firestore.js";
+
+class PostRepository {
+  #firestore;
+
+  constructor() {
+    this.firestore = db;
+  }
+
+  async getAllPosts() {
+    try {
+      const allPosts = await this.firestore.collection("posts").get();
+      const posts = [];
+
+      allPosts.forEach((post) => {
+        posts.push(post.data());
+      });
+      return posts;
+    } catch (error) {
+      throw new Error("Erro ao receber os posts");
+    }
+  }
+}
+export default PostRepository;
