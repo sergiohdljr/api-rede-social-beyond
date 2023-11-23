8da46a29fd1c255495d03075b9292b12b3425265
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-23 16:31:27 -0300

    styles: fix code coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: "node",
-  testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)*(spec|test).js?(x)"],
+  testMatch: ["**/?(*.)*(spec|test).js?(x)"],
   collectCoverage: true,
-  collectCoverageFrom: ["**/?(*.)*(spec|test).js?(x)"],
+  collectCoverageFrom: ["src/**/*.js"],
 };
