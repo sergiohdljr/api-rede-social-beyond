21ea733c5f2b1cd0388076a5ebc9e6a97f1aaefa
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-23 15:58:09 -0300

    tests: add code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.js?(x)", "**/?(*.)*(spec|test).js?(x)"],
+  collectCoverage: true,
+  collectCoverageFrom: ["**/?(*.)*(spec|test).js?(x)"],
 };
