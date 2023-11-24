19b2225e20b096d1996791bd2d187f6bbf37f741
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-24 15:32:24 -0300

    feat: create, delete, edit and findById methods on postRepository

--- src/repositories/PostRepository.js
+++ src/repositories/PostRepository.js
@@ -20,5 +20,40 @@ class PostRepository {
       throw new Error("Erro ao receber os posts");
     }
   }
+
+  async findById(id) {
+    try {
+      const post = await this.firestore.collection("posts").doc(id).get();
+      return post.data();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async createPost(post) {
+    const postRef = this.firestore.collection("posts").doc(post.id);
+    try {
+      await postRef.create(post);
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async deletePost(id) {
+    try {
+      await this.firestore.collection("posts").doc(id).delete();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  async edit(id, texto) {
+    try {
+      await this.firestore.collection("posts").doc(id).update({ texto });
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 }
+
 module.exports = PostRepository;
