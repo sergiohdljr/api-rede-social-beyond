38c73e93be1458b68ba41f1d7212385fd2c1b07b
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-24 18:31:37 -0300

    configurando rotas e controllers da aplicação

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const routes = require("./controllers/PostController");
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use("/api/v1", routes);
 
 module.exports = app;
---
+++ src/controllers/PostController.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const PostService = require("../service/PostService.js");
+
+const service = new PostService();
+const route = express.Router();
+
+route.get("/posts", async (req, res) => {
+  try {
+    const posts = await service.findPost();
+    return res.json(posts);
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
+});
+
+route.post("/post", async (req, res) => {
+  const post = req.body;
+  try {
+    if (!post || !post.id) {
+      throw new Error("body não foi definido.");
+    }
+
+    await service.createPost(post.id);
+    res.status(201).json({ message: "Post criado com sucesso!" });
+  } catch (error) {
+    res.status(400).json({ error: true, message: `Erro na criação do post: ${error.message}` });
+  }
+});
+
+route.delete("/post-delete/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await service.deletePost(id);
+    res.status(200).json({ message: `Post com id ${id} foi deletado com sucesso` });
+  } catch (error) {
+    res.status(400).json({ error: true, message: error.message });
+  }
+});
+
+route.put("/post-update/:id", async (req, res) => {
+  const { id } = req.params;
+  const { texto } = req.body;
+
+  try {
+    await service.updatePost(id, texto);
+    res.status(204).json({ message: `Post com id ${id} atualizado com sucesso!` });
+  } catch (error) {
+    res.status(500).json({ error: true, message: error.message });
+  }
+});
+
+module.exports = route;
