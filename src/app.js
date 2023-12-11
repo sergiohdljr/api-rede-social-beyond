9665b3fd6303a92abec97d9cb725131a93b7eee8
sergiohdljr <sergio.tjf1@gmail.com>
2023-12-11 12:41:39 -0300

    doc: generate docs with swagger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 const routes = require("./controllers/PostController");
 
 const app = express();
@@ -9,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1", routes);
 
 module.exports = app;
